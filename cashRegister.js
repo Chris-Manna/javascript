43ae80086e6659ec3302766f304236193bf5d138
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-11 19:10:17 -0500

    fixing it up

---
+++ cashRegister.js
@@ -0,0 +1,37 @@
+var cashRegister = {
+    total: 0,
+//insert the add method here    
+    this.add(itemCost) = function {
+        this.total += itemCost;
+    };
+    
+    scan: function (item) {
+        switch (item) { 
+        case "eggs": 
+            this.add(0.98); 
+            break;
+        
+        case "milk": 
+            this.add(1.23); 
+            break;
+        
+        //Add other 2 items here
+        case "magazine":
+            this.add(4.99);
+            break;
+        case "chocolate":
+            this.add(0.45);
+            break;
+        }
+        return true;
+    }
+};
+
+//Scan 2 eggs and 3 magazines
+ scan("eggs");
+ scan("eggs");
+ scan("magazine");
+ scan("magazine");
+ scan("magazine");
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
